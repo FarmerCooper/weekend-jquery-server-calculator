705bc0f5044c58804df50b640a89fa494f5cd97b
Diego Baeza <diegob12@live.com>
2022-06-25 19:49:13 -0500

    change button IDs to classes, create POST request to server in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,29 @@ $(document).ready(readyWhenever);
 
 function readyWhenever() {
     // console.log('jQuery is ready');
-    
+
+    $('.submit-button').on('click', handleSubmitBtn);
 }
+
+function handleSubmitBtn() {
+    // collect inputs
+
+    const newInput = {
+        numOne: $('#numOne').val(),
+        numTwo: $('#numTwo').val()
+    }
+
+    $.ajax({
+        url: '/calculations',
+        method: 'POST',
+        data: newInput // data here becomes req.body on the server
+    }).then(function(response) {
+        console.log(response);
+
+        //trigger a get
+    }).catch(function(error) {
+        console.log(error);
+        alert('Error in POST /calculations');
+    })
+};
+
